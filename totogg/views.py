f24f37d5418eb40bb6ff1568e7c27c9ec7b38d32
Junyoung Park <nhtrg1@gmail.com>
2022-06-30 14:57:46 +0900

    pred.html 템플릿 추가

--- totogg/views.py
+++ totogg/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def totogg(request):
   return render(request, 'totogg/home.html')
+
+def pred(request):
+  return render(request, 'totogg/pred.html')
